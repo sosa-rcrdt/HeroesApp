fcec23cb974ca040b5c4c48010c6961c0c83a578
Nelson <sosafco3@gmail.com>
2025-11-02 22:03:05 -0600

    Creación del método borrarHeroe en el services y su implementación

--- heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
+++ heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,16 +1,84 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+  `]
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ];
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: ''
+  }
+
+  constructor( private heroesService: HeroesService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router ) { }
 
   ngOnInit(): void {
+
+    if ( !this.router.url.includes('editar') ) {
+      return;
+    }
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.heroesService.getHeroePorId( id ) )
+      )
+      .subscribe( heroe => this.heroe = heroe );
+
+  }
+
+  guardar() {
+    if (this.heroe.superhero.trim().length === 0) {
+      return;
+    }
+
+    if ( this.heroe.id ) {
+      // Actualizar
+      this.heroesService.actualizarHeroe( this.heroe )
+        .subscribe( heroe =>
+          this.router.navigate(['/heroes/editar', heroe.id])
+        );
+    } else {
+      // Crear
+      this.heroesService.agregarHeroe( this.heroe )
+        .subscribe( heroe =>
+          this.router.navigate(['/heroes/editar', heroe.id])
+        );
+    }
+  }
+
+  borrar() {
+    this.heroesService.borrarHeroe( this.heroe.id! )
+      .subscribe( resp => {
+        this.router.navigate(['/heroes']);
+      });
   }
 
 }
--- heroesApp/src/app/heroes/pipes/imagen.pipe.ts
+++ heroesApp/src/app/heroes/pipes/imagen.pipe.ts
@@ -2,11 +2,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Pipe({
-  name: 'imagen'
+  name: 'imagen',
+  // pure: true
 })
 export class ImagenPipe implements PipeTransform {
 
   transform(heroe: Heroe): unknown {
+
+    if ( !heroe.id && !heroe.alt_img ) {
+      return 'assets/no-image.png';
+    } else if ( heroe.alt_img ) {
+      return heroe.alt_img;
+    }
+
     return `assets/heroes/${ heroe.id }.jpg`;
   }
 
